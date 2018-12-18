916a0b927179a5fd26176d2a52a57631864d829e
Raphael <raphaelrpais@gmail.com>
2018-12-18 12:04:00 -0200

    sai gsf entra fts

--- gsf-novo/_assets/js/home.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $( ".gsf-home" ).hover(function() {
-        $(this).children( ".gsf-home--bg" ).toggleClass( "gsf-home--bg__mouseover" );
-        $(this).children( ".gsf-home--text" ).toggleClass( "gsf-home--text__mouseover" );
-    });
-});
